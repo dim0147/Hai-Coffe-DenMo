19a496766926cd7812a1b250b8ee028d42803fe4
dim0147 <provandam0369@gmail.com>
2021-08-10 15:27:09 +0800

    refactor layout menu

--- lib/Screen/MenuScreen.dart
+++ lib/Screen/MenuScreen.dart
@@ -5,6 +5,105 @@ import 'package:hai_noob/App/Config.dart';
 import 'package:hai_noob/Controller/MenuController.dart';
 import 'package:hai_noob/Screen/Component.dart';
 
+class MenuScreen extends GetWidget<MenuController> {
+  const MenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu'),
+        ),
+        drawer: NavigateMenu(),
+        bottomNavigationBar: Footer(),
+        body: Container(
+          child: Obx(() => controller.isLoading.value
+              ? SizedBox.expand(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(width: 10.0),
+                      Text('Đang tải dữ liệu')
+                    ],
+                  ),
+                )
+              : SingleChildScrollView(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CategoryBtns(),
+                      RightPanel(),
+                    ],
+                  ),
+                )),
+        ),
+      ),
+    );
+  }
+}
+
+class RightPanel extends StatelessWidget {
+  const RightPanel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          spacing: 8.0,
+          runSpacing: 8.0,
+          children: [
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+            MenuItem(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryBtns extends StatelessWidget {
+  const CategoryBtns({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          OutlinedButton(
+            onPressed: () {},
+            child: Text('Đồ Ăn'),
+            style: OutlinedButton.styleFrom(
+                backgroundColor: Get.theme.primaryColor.withOpacity(0.5)),
+          ),
+          OutlinedButton(onPressed: () {}, child: Text('Đồ Ăn')),
+          OutlinedButton(onPressed: () {}, child: Text('Đồ Ăn')),
+          OutlinedButton(onPressed: () {}, child: Text('Đồ Ăn')),
+          OutlinedButton(onPressed: () {}, child: Text('Đồ Ăn')),
+        ],
+      ),
+    );
+  }
+}
+
 class MenuItem extends GetWidget<MenuController> {
   const MenuItem({Key? key}) : super(key: key);
 
@@ -76,155 +175,81 @@ class MenuItem extends GetWidget<MenuController> {
   }
 }
 
-class MenuScreen extends GetWidget<MenuController> {
-  const MenuScreen({Key? key}) : super(key: key);
+class Footer extends StatelessWidget {
+  const Footer({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Menu'),
-        ),
-        drawer: NavigateMenu(),
-        body: Container(
-          child: Obx(() => controller.isLoading.value
-              ? SizedBox.expand(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CircularProgressIndicator(),
-                      SizedBox(width: 10.0),
-                      Text('Đang tải dữ liệu')
-                    ],
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.orangeAccent[200],
+      ),
+      child: Row(
+        children: [
+          // Cart quality badge
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.deepPurpleAccent,
+                ),
+              ),
+              Positioned(
+                child: new Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: new BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: BorderRadius.circular(6),
                   ),
-                )
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    TabBar(
-                      isScrollable: true,
-                      indicatorColor: Get.theme.primaryColor,
-                      tabs: controller.tabs,
-                      controller: controller.tabController,
-                      indicatorSize: TabBarIndicatorSize.tab,
+                  constraints: BoxConstraints(
+                    minWidth: 18,
+                    minHeight: 18,
+                  ),
+                  child: Text(
+                    '5',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
                     ),
-                    Expanded(
-                      child: TabBarView(
-                        children: [
-                          // Tabs
-                          SingleChildScrollView(
-                            child: Container(
-                              padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Wrap(
-                                      spacing: 10.0,
-                                      runSpacing: 10.0,
-                                      children: [
-                                        MenuItem(),
-                                      ],
-                                    ),
-                                  ]),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Wrap(
-                                    spacing: 10.0,
-                                    runSpacing: 10.0,
-                                    children: [
-                                      MenuItem(),
-                                    ],
-                                  ),
-                                ]),
-                          )
-                        ],
-                        controller: controller.tabController,
-                      ),
-                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              )
+            ],
+          ),
 
-                    // Footer
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      height: 60,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent[200],
-                      ),
-                      child: Row(
-                        children: [
-                          // Cart quality badge
-                          Stack(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.shopping_cart,
-                                  color: Colors.deepPurpleAccent,
-                                ),
-                              ),
-                              Positioned(
-                                child: new Container(
-                                  padding: EdgeInsets.all(2),
-                                  decoration: new BoxDecoration(
-                                    color: Colors.deepPurpleAccent,
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                  constraints: BoxConstraints(
-                                    minWidth: 18,
-                                    minHeight: 18,
-                                  ),
-                                  child: Text(
-                                    '5',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 12,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
+          // Select table
+          Expanded(
+            flex: 3,
+            child: OutlinedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.table_chart),
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(
+                    width: 2.0, color: Get.theme.primaryColor.withOpacity(0.5)),
+              ),
+              label: Text('Chọn Bàn'),
+            ),
+          ),
 
-                          // Select table
-                          Expanded(
-                            flex: 3,
-                            child: OutlinedButton.icon(
-                              onPressed: () {},
-                              icon: Icon(Icons.table_chart),
-                              style: OutlinedButton.styleFrom(
-                                side: BorderSide(
-                                    width: 2.0,
-                                    color: Get.theme.primaryColor
-                                        .withOpacity(0.5)),
-                              ),
-                              label: Text('Chọn Bàn'),
-                            ),
-                          ),
+          SizedBox(width: 5),
 
-                          SizedBox(width: 5),
-
-                          // Payment
-                          Expanded(
-                            flex: 3,
-                            child: ElevatedButton.icon(
-                              onPressed: () {},
-                              icon: Icon(Icons.payment),
-                              label: Text('Payment'),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )),
-        ),
+          // Payment
+          Expanded(
+            flex: 3,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.payment),
+              label: Text('Payment'),
+            ),
+          )
+        ],
       ),
     );
   }
