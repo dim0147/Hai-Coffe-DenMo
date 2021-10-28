b801ebc92da3968d58d3cc411d424272991e8404
dim0147 <provandam0369@gmail.com>
2021-10-28 10:25:04 +0800

    Refactor createCategoryScreen/Controller

--- lib/Screen/Category/CreateCategoryScreen.dart
+++ lib/Screen/Category/CreateCategoryScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hai_noob/App/Utils.dart';
 
 import '../../App/Config.dart';
 
@@ -18,66 +19,61 @@ class CreateCategoryScreen extends GetView<CreateCategoryController> {
           child: Center(
             child: Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Tên danh mục',
-                      prefixIcon: Icon(
-                        Icons.category,
-                      ),
-                    ),
-                    controller: controller.nameC,
-                  ),
-                ),
+                Title(),
 
                 // Create Button
-                controller.obx(
-                  (state) => Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Text(
-                          state ?? '',
-                          style: TextStyle(
-                            color: Colors.green,
-                          ),
-                        ),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: controller.onCreate,
-                        icon: Icon(Icons.add),
-                        label: Text('Tạo'),
-                      )
-                    ],
-                  ),
-                  onLoading: CircularProgressIndicator(
-                    color: AppConfig.MAIN_COLOR,
-                  ),
-                  onError: (err) => Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Text(
-                          err ?? 'Có lỗi xảy ra',
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                      ),
-                      TextButton.icon(
-                        onPressed: controller.onCreate,
-                        icon: Icon(Icons.add),
-                        label: Text('Tạo'),
-                      )
-                    ],
-                  ),
-                ),
+                CreateBtn(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class Title extends GetView<CreateCategoryController> {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Tên danh mục',
+          prefixIcon: Icon(
+            Icons.category,
+          ),
+        ),
+        controller: controller.nameC,
+      ),
+    );
+  }
+}
+
+class CreateBtn extends GetView<CreateCategoryController> {
+  const CreateBtn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () {
+        final Widget? cStateWidget =
+            Utils.cStateInLoadingOrError(controller.cState.value);
+        if (cStateWidget != null) return cStateWidget;
+        return Column(
+          children: [
+            ElevatedButton.icon(
+              onPressed: controller.onCreate,
+              icon: Icon(Icons.add),
+              label: Text('Tạo'),
+            )
+          ],
+        );
+      },
     );
   }
 }
