a2095fdc58c64f5a705d27d9923a289b743bdec0
dim0147 <provandam0369@gmail.com>
2021-10-31 14:18:05 +0800

    update gitignore

--- lib/DAO/ItemDAO.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'ItemDAO.dart';
-
-// **************************************************************************
-// DaoGenerator
-// **************************************************************************
-
-mixin _$ItemsDAOMixin on DatabaseAccessor<AppDatabase> {}
